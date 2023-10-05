a38730acc7ec58842eba47d270ef091eaf0a08ae
Anand Choudhary <79297451+andy-joy-25@users.noreply.github.com>
2023-10-05 15:39:16 +0200

    Update subgradient_mae.py

--- labs/ex02/template/subgradient_mae.py
+++ labs/ex02/template/subgradient_mae.py
@@ -16,4 +16,10 @@ def compute_subgradient_mae(y, tx, w):
     # INSERT YOUR CODE HERE
     # TODO: compute subgradient gradient vector for MAE
     # ***************************************************
-    raise NotImplementedError
+    r = np.random.randint(-1,2) #Return a random no. from [-1,1]
+    del_h = y - np.matmul(tx,w)
+    del_h[del_h < 0] = -1
+    del_h[del_h == 0] = r
+    del_h[del_h > 0] = 1
+    return -(1/y.shape[0])*np.matmul(tx.T,del_h)
+
