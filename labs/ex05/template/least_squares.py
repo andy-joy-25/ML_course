32a469e0b1f94173c47ddd9e0efa2899ce9bd855
Anand Choudhary <79297451+andy-joy-25@users.noreply.github.com>
2023-11-05 23:09:32 +0100

    Update least_squares.py

--- labs/ex05/template/least_squares.py
+++ labs/ex05/template/least_squares.py
@@ -14,4 +14,8 @@ def least_squares(y, tx):
     # least squares: TODO
     # returns mse, and optimal weights
     # ***************************************************
-    raise NotImplementedError
+    w = np.linalg.solve(np.matmul(tx.T,tx),np.matmul(tx.T,y))
+    
+    mse = (0.5*(1/y.shape[0])*np.matmul((y - np.matmul(tx,w)).T,(y - np.matmul(tx,w)))).item()
+    
+    return w,mse
